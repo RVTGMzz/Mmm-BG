f96e609bb91aac5f73fa34bc47fc46145b2bd7f4
ronvotri <lengochung28191@gmail.com>
2026-09-13 21:58:04 +0700

    feat(0.1.26): activate Turn Stakes board scene

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { LocalLobbyScene } from './scenes/LocalLobbyScene';
 import { SetupScene } from './scenes/SetupScene';
 import { BoardScene } from './scenes/BoardScene';
 import { NetworkBoardScene } from './scenes/NetworkBoardScene';
-import { PartyMechanicsBoardScene } from './scenes/PartyMechanicsBoardScene';
+import { TurnStakesBoardScene } from './scenes/TurnStakesBoardScene';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
@@ -15,7 +15,7 @@ const config: Phaser.Types.Core.GameConfig = {
   width: 1280,
   height: 720,
   backgroundColor: '#f4ead7',
-  scene: [LocalLobbyScene, SetupScene, PartyMechanicsBoardScene, BoardScene, NetworkBoardScene],
+  scene: [LocalLobbyScene, SetupScene, TurnStakesBoardScene, BoardScene, NetworkBoardScene],
   dom: {
     createContainer: true,
   },
