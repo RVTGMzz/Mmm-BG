a6e069dc62c555604a59aae4a8ae1a9635f2e24b
ronvotri <lengochung28191@gmail.com>
2026-09-15 10:18:35 +0700

    feat(0.1.61): activate local playtest report runtime

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { SetupScene } from './scenes/SetupScene';
 import { TurnOrderScene048 as TurnOrderScene } from './scenes/TurnOrderScene048';
 import { BoardScene } from './scenes/BoardScene';
 import { NetworkBoardScene } from './scenes/NetworkBoardScene';
-import { CareerMinigameBoardScene060 as ActiveBoardScene } from './scenes/CareerMinigameBoardScene060';
+import { CareerMinigameBoardScene061 as ActiveBoardScene } from './scenes/CareerMinigameBoardScene061';
 import { FinalMapPreviewScene050 } from './scenes/FinalMapPreviewScene050';
 import { FinalMapPreviewScene051 } from './scenes/FinalMapPreviewScene051';
 import { FinalMapPreviewScene052 } from './scenes/FinalMapPreviewScene052';
